a5a74fe9fde2055309f29b624d3e88e15fec07bd
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-01-25 23:57:49 +0000

    Fix some linting stuff
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- features/test/aws/test.sh
+++ features/test/aws/test.sh
@@ -11,6 +11,7 @@ check "aws featurerc existence" stat /home/vscode/.devcontainer/featurerc.d/aws.
 
 check "aws-sso version" aws-sso version
 check "aws-sso featurerc existence" stat /home/vscode/.devcontainer/featurerc.d/aws-sso.sh
+# shellcheck disable=SC2034 exit status should always evaluate as expected
 check "aws-sso featurerc password" source /home/vscode/.devcontainer/featurerc.d/aws-sso.sh && [[ "${AWS_SSO_FILE_PASSWORD}" == "REPLACE_ME" ]] && exit 1 || true
 check "aws-sso configuration existence" stat /home/vscode/.aws-sso/config.yaml
 
