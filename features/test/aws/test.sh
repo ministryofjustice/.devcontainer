b5dda2bb4d4489d7e66111624748bb045738f299
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-01-26 11:09:16 +0000

    Fix Dockerfile and hopefully shellcheck
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- features/test/aws/test.sh
+++ features/test/aws/test.sh
@@ -12,7 +12,7 @@ check "aws featurerc existence" stat /home/vscode/.devcontainer/featurerc.d/aws.
 check "aws-sso version" aws-sso version
 check "aws-sso configuration existence" stat /home/vscode/.aws-sso/config.yaml
 check "aws-sso featurerc existence" stat /home/vscode/.devcontainer/featurerc.d/aws-sso.sh
-# shellcheck disable=SC2034 exit status should always evaluate as expected
+# shellcheck disable=SC2034
 check "aws-sso featurerc password" source /home/vscode/.devcontainer/featurerc.d/aws-sso.sh && [[ "${AWS_SSO_FILE_PASSWORD}" == "REPLACE_ME" ]] && exit 1 || true
 check "aws-sso promptrc existence" stat /home/vscode/.devcontainer/promptrc.d/aws-sso.sh
 
