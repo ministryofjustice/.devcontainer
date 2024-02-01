82d6da7372d2004065fb431142da9d4c2504c92c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-02-01 17:11:55 +0000

    Fix Cloud Platform prompt
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- features/src/cloud-platform/src/home/vscode/.devcontainer/promptrc.d/cloud-platform.sh
+++ features/src/cloud-platform/src/home/vscode/.devcontainer/promptrc.d/cloud-platform.sh
@@ -6,7 +6,7 @@ PROMPT+='`\
   kubectlCurrentNamespace=$(kubectl config view --minify --output "jsonpath={..namespace}"); \
   kubectlClientId=$(kubectl config view --output json | jq -r ".users[0].user[\"auth-provider\"].config[\"client-id\"]"); \
   kubectlServerVersion=$(kubectl version --output json 2>/dev/null | jq -r .serverVersion.gitVersion | sed "s/v//"); \
-  && if [[ "${kubectlCurrentContext}" == "cloud-platform-live" ]] && [[ "${kubectlClientId}" == "REPLACE_WITH_CLIENT_ID" ]]; then \
+  if [[ "${kubectlCurrentContext}" == "cloud-platform-live" ]] && [[ "${kubectlClientId}" == "REPLACE_WITH_CLIENT_ID" ]]; then \
     echo -n "[ cluster: %{$fg[yellow]%}${kubectlCurrentContext} (requires authentication)%{$reset_color%} ] "; \
   elif [[ "${kubectlCurrentContext}" == "cloud-platform-live" ]] && [[ ! -z "${kubectlServerVersion}" ]]; then \
     echo -n "[ cluster: %{$fg[green]%}${kubectlCurrentContext} (authenticated)%{$reset_color%} ] "; \
