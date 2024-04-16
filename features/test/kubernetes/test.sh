a11567cc30c71c20aacd331cd72efe58a78f8779
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-04-16 16:07:54 +0100

    🗺️ Add Helm and Helm Chart Testing (#59)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- features/test/kubernetes/test.sh
+++ features/test/kubernetes/test.sh
@@ -8,5 +8,10 @@ source dev-container-features-test-lib
 
 check "kubectl version" kubectl version --client=true --output yaml
 check "kubectl featurerc existence" stat /home/vscode/.devcontainer/featurerc.d/kubectl.sh
+check "helm version" helm version
+check "helm featurerc existence" stat /home/vscode/.devcontainer/featurerc.d/helm.sh
+check "helm ct version" ct version
+check "helm chart_schema.yaml existence" stat /home/vscode/.ct/chart_schema.yaml
+check "helm lintconf.yaml existence" stat /home/vscode/.ct/lintconf.yaml
 
 reportResults
