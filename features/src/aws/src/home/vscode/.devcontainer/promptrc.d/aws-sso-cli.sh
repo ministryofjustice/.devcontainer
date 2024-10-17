44a12595f6b0618730b3c923e38d93d77b4f5676
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-10-17 21:20:43 +0000

    Update AWS SSO
    Signed-off-by: GitHub <noreply@github.com>

--- features/src/aws/src/home/vscode/.devcontainer/promptrc.d/aws-sso-cli.sh
+++ features/src/aws/src/home/vscode/.devcontainer/promptrc.d/aws-sso-cli.sh
@@ -2,13 +2,13 @@
 # shellcheck disable=SC2016
 
 PROMPT+='`\
-  if [[ ${AWS_SSO_PROFILE} == *"development"* || ${AWS_SSO_PROFILE} == *"test"* ]]; then \
+  if [[ ${AWS_SSO_PROFILE} == *"development"* ]]; then \
     echo -n "[ aws: %{$fg[green]%}${AWS_SSO_PROFILE}@${AWS_DEFAULT_REGION}%{$reset_color%} ] "; \
+  elif [[ ${AWS_SSO_PROFILE} == *"test"* ]]; then \
+    echo -n "[ aws: %{$fg[blue]%}${AWS_SSO_PROFILE}@${AWS_DEFAULT_REGION}%{$reset_color%} ] "; \
   elif [[ ${AWS_SSO_PROFILE} == *"preproduction"* ]]; then \
     echo -n "[ aws: %{$fg[yellow]%}${AWS_SSO_PROFILE}@${AWS_DEFAULT_REGION}%{$reset_color%} ] "; \
   elif [[ ${AWS_SSO_PROFILE} == *"production"* ]]; then \
     echo -n "[ aws: %{$fg[red]%}${AWS_SSO_PROFILE}@${AWS_DEFAULT_REGION}%{$reset_color%} ] "; \
-  elif [[ ! -z ${AWS_SSO_PROFILE} ]]; then \
-    echo -n "[ aws: %{$fg[blue]%}${AWS_SSO_PROFILE}@${AWS_DEFAULT_REGION}%{$reset_color%} ] "; \
-  fi \
+  fi
 `'
