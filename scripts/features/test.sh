920b422a6a0c8c0ef4452055f435751a7c417598
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-10-17 20:22:22 +0000

    shell fmt
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/features/test.sh
+++ scripts/features/test.sh
@@ -7,7 +7,7 @@ docker build \
   --tag ghcr.io/ministryofjustice/devcontainer-base:local-feature-test \
   images/base
 
-# devcontainer features test \
+devcontainer features test \
   --project-folder features \
   --features "${FEATURE_TO_TEST}" \
   --skip-scenarios \
