eafbffac67172a5cf42b9b7362c45148f2826b39
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-04-22 12:48:50 +0000

    update install.sh

--- features/src/apm/install.sh
+++ features/src/apm/install.sh
@@ -8,8 +8,8 @@ logger "info" "Installing apm (version: ${APMVERSION})"
 bash "$(dirname "${0}")"/install-apm.sh
 
 if [ -f "apm.yml" ]; then
-    logger "info" "Installing apm packages defined in apm.yml"
-    apm install
+  logger "info" "Installing apm packages defined in apm.yml"
+  apm install
 else
-    logger "info" "No apm.yml found in $(pwd); skipping apm package installation"
+  logger "info" "No apm.yml found in $(pwd); skipping apm package installation"
 fi
